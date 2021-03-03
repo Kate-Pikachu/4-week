b9323aa57eae38d1405f4d5e93326a84c9d98bf5
Kate-Pikachu <pikulina.ev@phystech.edu>
2021-03-04 00:21:27 +0300

    создали последовательность П1 целых чисел

--- task/main.cpp
+++ task/main.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
+#include <vector>
 
 int main() {
-	using namespace std;
+
+	std::cout << "1) create a sequence P1" << std::endl;
+	std::vector <int> P1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	for (auto const& element : P1)
+		std::cout << element << ' ';
+
 
 
 	system("pause");
