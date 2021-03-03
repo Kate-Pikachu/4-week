8508ff32544cf4b70c2f854346107a980acac651
Kate-Pikachu <pikulina.ev@phystech.edu>
2021-03-04 00:47:22 +0300

    добавили элементы в П1

--- task/main.cpp
+++ task/main.cpp
@@ -1,12 +1,23 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
+
+void show_sequence(std::vector<int> vec) {
+	for (auto const& element : vec)
+		std::cout << element << ' ';
+}
 
 int main() {
 
 	std::cout << "1) create a sequence P1" << std::endl;
 	std::vector <int> P1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	for (auto const& element : P1)
-		std::cout << element << ' ';
+	show_sequence(P1);
+	std::cout << std::endl;
+
+	std::cout << "2) add some numbers in P1" << std::endl;
+	P1.push_back(11);
+	P1.push_back(12);
+	show_sequence(P1);
+	std::cout << std::endl;
 
 
 
